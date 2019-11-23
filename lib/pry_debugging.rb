e651c0b64add521995774492d7f2922edcb5aebd
Jamal Rjoul <jamensky1991@gmail.com>
2019-11-23 18:18:06 +0000

    Automatically backed up by Learn

--- lib/pry_debugging.rb
+++ lib/pry_debugging.rb
@@ -1,3 +1,26 @@
-def plus_two(num)
-	return num + 2
+require'pry'
+
+contacts = {
+     "Jon Snow" => {
+       name: "Jon",
+       email: "jon_snow@thewall.we",
+       favorite_ice_cream_flavors: ["chocolate", "vanilla"]
+     },
+     "Freddy Mercury" => {
+       name: "Freddy",
+       email: "freddy@mercury.com",
+       favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
+    }
+   }
+
+def remove_strawberry(contacts)
+   contacts["Freddy Mercury"].each do |fredM, value|
+      value.each do |key, value|
+
+      binding.pry
+   end
+   end
+
+
+
 end
